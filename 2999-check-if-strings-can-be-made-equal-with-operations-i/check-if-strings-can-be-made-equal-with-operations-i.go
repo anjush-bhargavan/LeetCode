afc100d46ab4eaa25f62694630a38490296dc327
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-01-30 09:54:28 +0530

    Time: 6 ms (13.64%) | Memory: 2.3 MB (13.64%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.go
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.go
@@ -1,12 +1,7 @@
-func canBeEqual(s1 string, s2 string) bool {
-    
+func canBeEqual(s1 string, s2 string) bool { 
     for i := 0 ; i < 2 ; i ++ {
-        if s1[i] != s2[i] && s1[i] != s2[i+2] {
-            return false
-        }
-    }
-    for i := 2 ; i < 4 ; i ++ {
-        if s1[i] != s2[i-2] && s1[i] != s2[i] {
+        s := string(s1[i]) + string(s1[i+2])
+        if s != string(s2[i])+string(s2[i+2]) && s != string(s2[i+2])+string(s2[i]) {
             return false
         }
     }
