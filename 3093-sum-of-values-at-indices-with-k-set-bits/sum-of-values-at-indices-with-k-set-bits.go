6acf5e2f62406140ad075222be1208a5a3ae7029
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-02-01 11:02:04 +0530

    Time: 11 ms (16.80%) | Memory: 4.6 MB (8.00%) - LeetSync

--- 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.go
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.go
@@ -2,13 +2,8 @@ func sumIndicesWithKSetBits(nums []int, k int) int {
     sum := 0
     for i := 0 ; i < len(nums) ; i++ {
         bStr := strconv.FormatInt(int64(i),2)
-        count := 0
-        for _,s := range bStr {
-           if s == '1'{
-               count++
-           }
-        }
-        if count == k {
+        s := strings.Replace(bStr,"0","",-1)
+        if len(s) == k {
             sum += nums[i]
         }
     }
