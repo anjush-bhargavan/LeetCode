ce419621bbbd1ec419198144b11fe8ad0d0454fe
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-02-02 15:36:11 +0530

    Time: 20 ms (25.78%) | Memory: 7.3 MB (10.55%) - LeetSync

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	curr := root
+    for{
+        if p.Val < curr.Val && q.Val < curr.Val {
+            curr = curr.Left
+        }else if p.Val > curr.Val && q.Val > curr.Val{
+            curr = curr.Right
+        }else{
+            break
+        } 
+    }
+    return curr
+}
