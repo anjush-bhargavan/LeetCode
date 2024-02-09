e7c6ca7c7b57618e0313936397093af8a08b6203
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-02-09 09:43:03 +0530

    Time: 6 ms (9.20%) | Memory: 3 MB (77.01%) - LeetSync

---
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.go
@@ -0,0 +1,14 @@
+func findGCD(nums []int) int {
+    sort.Ints(nums)
+    min,max := nums[0],nums[len(nums)-1]
+    for i := max ; i >= 1 ; i-- {
+        if Divisor(min,max,i){
+            return i
+        } 
+    }
+    return 1
+}
+
+func Divisor(a,b,num int) bool {
+    return a%num == 0 && b%num == 0
+}
