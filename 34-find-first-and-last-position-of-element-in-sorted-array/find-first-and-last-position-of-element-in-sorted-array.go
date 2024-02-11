e1103328d45108d6f09e88fb192578dc4a44d09c
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-02-11 08:27:18 +0530

    Time: 3 ms (93.38%) | Memory: 4.6 MB (18.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.go
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.go
@@ -1,14 +1,29 @@
 func searchRange(nums []int, target int) []int {
-    first,last,flag := -1,-1,false
-    for i := 0 ; i <len(nums) ; i++ {
-        if nums[i] == target && !flag{
-            first = i
-            flag = true
-        }
-        if nums[i] == target && flag{
-            last = i
-            flag = true
+    low,high,idx := 0 , len(nums)-1 , -1
+    for low <= high {
+        mid := (low+high)/2
+        if nums[mid] == target {
+            idx = mid
+            break
+        }else if target < nums[mid] {
+            high = mid - 1
+        }else {
+            low = mid + 1
         }
     }
-    return []int{first,last}
+    last,first := idx,idx
+    if idx == -1 {
+        return []int{first,last}
+    }
+
+    for {
+        if first >= 0 &&  nums[first] == target{
+            first--
+        }else if  last < len(nums) && nums[last] == target {
+            last++
+        }else {
+            break
+        }
+    }
+    return []int{first+1,last-1}
 }
