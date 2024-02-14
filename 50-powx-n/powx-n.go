d5b5277191a8b3ad1ed5709051c35bb306b3d63d
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-02-14 09:51:52 +0530

    Time: 2 ms (60.70%) | Memory: 2 MB (52.74%) - LeetSync

--- 50-powx-n/powx-n.go
+++ 50-powx-n/powx-n.go
@@ -1,23 +1,19 @@
 func myPow(x float64, n int) float64 {
-    result,flag,i := x,false,1
-    if x == 1.00 || n == 0{
+    if n < 0 {
+        return 1/pow(x,-n)
+    }
+ 
+    return pow(x,n)
+}
+
+func pow(x float64,n int) float64 {
+    if n == 0 {
         return 1
     }
-    if n < 0 {
-        flag = true
-        n *= -1
-    }
-    for 2*i <= n {
-        result *= result
-        i *= 2
-
-    }
-    for i < n {
+    result := pow(x,n/2)
+    result *= result
+    if n%2 == 1 {
         result *= x
-        i++
-    }
-    if flag {
-        return 1/result
     }
     return result
 }
