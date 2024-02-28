14c9d62d1721ab0d06db6bec781fb8d7452f41b6
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-02-28 10:38:00 +0530

    Time: 7 ms (46.15%) | Memory: 5.5 MB (92.31%) - LeetSync

---
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.go
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findBottomLeftValue(root *TreeNode) int {
+    maxCount,count,min := -1,0,0
+  
+    LeftVal(root,&count,&maxCount,&min)
+
+    return min
+}
+
+
+func LeftVal(node *TreeNode,count,maxCount,min *int) {
+    if node.Left == nil && node.Right == nil {
+        fmt.Println(*count,node.Val)
+        if *count > *maxCount {
+            *min = node.Val
+            *maxCount = *count
+        }
+        return
+    }
+
+    *count++
+    if node.Left != nil && node.Right != nil  {
+        LeftVal(node.Left,count,maxCount,min)
+        LeftVal(node.Right,count,maxCount,min)
+    }else if node.Left != nil {
+         LeftVal(node.Left,count,maxCount,min)
+    }else{
+        LeftVal(node.Right,count,maxCount,min)
+    }
+   *count--
+    
+}
