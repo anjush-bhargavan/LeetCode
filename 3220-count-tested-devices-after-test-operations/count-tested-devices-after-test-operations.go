5d6e4cde0ea82831f61ee38195450865a799e034
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-02-29 19:20:55 +0530

    Time: 3 ms (71.13%) | Memory: 2.8 MB (75.26%) - LeetSync

---
+++ 3220-count-tested-devices-after-test-operations/count-tested-devices-after-test-operations.go
@@ -0,0 +1,10 @@
+func countTestedDevices(batteryPercentages []int) int {
+    count := 0
+
+    for i := 0 ; i < len(batteryPercentages) ; i++ {
+        if batteryPercentages[i] - count > 0 {
+            count++
+        }
+    }
+    return count
+}
