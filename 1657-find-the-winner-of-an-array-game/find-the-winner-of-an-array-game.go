459aa2387ae0cb0b1ce49838ee2172ef3d202e2e
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-04 18:30:48 +0530

    Time: 83 ms (63.16%) | Memory: 11.4 MB (15.79%) - LeetSync

---
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.go
@@ -0,0 +1,19 @@
+func getWinner(arr []int, k int) int {
+    count,winner := 0,arr[0] 
+    for i := 0 ; i < len(arr) ; i++ {
+        if arr[0] > arr[1] {
+            arr[0],arr[1] = arr[1],arr[0]
+        }
+            arr = append(arr,arr[0])
+            arr = arr[1:]
+            count++
+            if winner != arr[0] {
+                count = 1 
+                winner = arr[0]
+            }
+            if count == k {
+                return winner
+            }
+    }
+    return arr[0]
+}
