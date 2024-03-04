dd6525fa4677692992797c35ad3f2a171056b9b1
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-04 18:33:06 +0530

    Time: 81 ms (68.42%) | Memory: 9 MB (36.84%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.go
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.go
@@ -1,5 +1,9 @@
 func getWinner(arr []int, k int) int {
-    count,winner := 0,arr[0] 
+    count,winner := 0,arr[0]
+    if k >= len(arr) {
+        sort.Ints(arr)
+        return arr[len(arr)-1]
+    } 
     for i := 0 ; i < len(arr) ; i++ {
         if arr[0] > arr[1] {
             arr[0],arr[1] = arr[1],arr[0]
