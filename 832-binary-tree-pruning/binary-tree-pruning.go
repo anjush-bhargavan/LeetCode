45a3f3a1dfca7ed793fe39e8e8e9fd055a62b2ea
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-07 09:45:28 +0530

    Time: 0 ms (100.00%) | Memory: 2.3 MB (5.71%) - LeetSync

---
+++ 832-binary-tree-pruning/binary-tree-pruning.go
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pruneTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    root.Left = pruneTree(root.Left)
+    root.Right = pruneTree(root.Right)
+
+    if root.Val == 0 && root.Right == nil && root.Left == nil {
+        return nil
+    }
+    
+    return root
+}
+
