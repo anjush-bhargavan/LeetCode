5e483b98832feac60c28578df97102756fcdfdca
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-11 15:08:30 +0530

    Time: 193 ms (75.80%) | Memory: 15 MB (78.43%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.go
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.go
@@ -1,53 +1,22 @@
 func findMinArrowShots(points [][]int) int {
-    arr := MergeSort(points)
-    count, min,max := 1,arr[0][0],arr[0][1]
-    for i := 1 ; i < len(arr) ; i++ {
-        if arr[i][0] <= max {
-            if max > arr[i][1] {
-                max = arr[i][1]
-            }
-            if min < arr[i][0] {
-                min = arr[i][0]
+    Sort2D(points)
+    count,max := 1,points[0][1]
+    for i := 1 ; i < len(points) ; i++ {
+        if points[i][0] <= max {
+            if max > points[i][1] {
+                max = points[i][1]
             }
         }else{
-            max = arr[i][1]
-            min = arr[i][0]
+            max = points[i][1]
             count++
         }
     }
     return count 
 }
 
-func MergeSort(arr [][]int) [][]int {
-    if len(arr) == 1 {
-        return arr
-    }
-    mid := len(arr)/2
-    left := MergeSort(arr[:mid])
-    right := MergeSort(arr[mid:])
+func Sort2D(arr [][]int) {
+    sort.Slice(arr, func(i, j int) bool {
+		return arr[i][1] < arr[j][1]
+	})
 
-    return merge(left,right)
-}
-
-func merge(arr1,arr2 [][]int) [][]int {
-	combined:=[][]int{}
-	i,j:=0,0
-	for i<len(arr1) && j <len(arr2){
-		if arr1[i][0] < arr2[j][0]{
-            combined = append(combined,arr1[i])
-            i++
-		}else{
-			combined = append(combined,arr2[j])
-            j++
-		}
-	}
-	for i < len(arr1){
-	    combined = append(combined,arr1[i])
-        i++
-	}
-	for j < len(arr2){
-		combined = append(combined,arr2[j])
-        j++
-	}
-	return combined
 }
