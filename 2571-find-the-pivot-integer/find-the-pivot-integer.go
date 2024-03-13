0b2ed48e57d69f7540e6be5c2e48858a8da52434
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-13 12:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 2 MB (100.00%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.go
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.go
@@ -1,12 +1,9 @@
 func pivotInteger(n int) int {
-    i,j:= 1,n
-    if n == 1 {
-        return 1
-    }
-    sum := (n+2)*(n-1)/2
+    i,j := 0,n
+    sum := (n + 2) * (n - 1) / 2
     for i <= j {
-        mid := (i+j)/2
-        pivot := (mid*(mid+2))
+        mid := (i + j) / 2
+        pivot := (mid * (mid + 2))
         if pivot == sum {
             return mid + 1
         }else if pivot < sum {
