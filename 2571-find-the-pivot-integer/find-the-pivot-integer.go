d4eab82102795829687a0157a07082e60720ffea
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-13 11:36:17 +0530

    Time: 2 ms (65.00%) | Memory: 2 MB (25.00%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.go
@@ -0,0 +1,19 @@
+func pivotInteger(n int) int {
+    i,j:= 1,n
+    if n == 1 {
+        return 1
+    }
+    sum := (n+2)*(n-1)/2
+    for i <= j {
+        mid := (i+j)/2
+        pivot := (mid*(mid+2))
+        if pivot == sum {
+            return mid + 1
+        }else if pivot < sum {
+            i = mid + 1
+        }else{
+            j = mid -1
+        }
+    } 
+    return -1
+}
