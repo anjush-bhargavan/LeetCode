7bfc5f064607ae57f3821eb9b1289fd99311e087
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-22 09:48:49 +0530

    Time: 46 ms (55.59%) | Memory: 7.4 MB (83.39%) - LeetSync

--- 15-3sum/3sum.go
+++ 15-3sum/3sum.go
@@ -2,11 +2,14 @@ func threeSum(nums []int) [][]int {
     result := [][]int{}
     sort.Ints(nums)
     for i := 0 ; i < len(nums) ; i++ {
-    j :=  i + 1
-    k := len(nums) - 1
+
+        j :=  i + 1
+        k := len(nums) - 1
+
         for i < len(nums)-1 && nums[i] == nums[i+1] {
             i++
         }
+        
         for j < k {
             v :=  nums[i] + nums[j] + nums[k]  
             if v == 0  {
@@ -32,37 +35,3 @@ func threeSum(nums []int) [][]int {
     }
     return result  
 }
-
-
-
-                // pairMap := make(map[int]bool)
-                // pairMap[nums[i]] = true
-                // pairMap[nums[j]] = true
-                // pairMap[nums[k]] = true
-                // jsonData,_ := json.Marshal(pairMap)
-                // jsonString := string(jsonData)
-                // if !tripletMap[jsonString] {
-                //     tripletMap[jsonString] = true 
-                // }else{
-                //     break
-                // }
-
-
-        // for j := i+1 ; j < len(nums) ; j++ {
-        //     v := 0 - (nums[i]+nums[j])
-        //     if numMap[v] {
-        //         pairMap := make(map[int]bool)
-        //         pairMap[nums[i]] = true
-        //         pairMap[nums[j]] = true
-        //         pairMap[v] = true
-        //         jsonData,_ := json.Marshal(pairMap)
-        //         jsonString := string(jsonData)
-        //         if !tripletMap[jsonString] {
-        //             tripletMap[jsonString] = true 
-        //             temp := []int{nums[i],nums[j],v}
-        //             result = append(result,temp)
-        //         }else{
-        //             break
-        //         }
-        //     }
-        // } 
