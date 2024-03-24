a78288e81700ed91fc935083ef83dbaaaf386e7a
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-24 13:18:53 +0530

    Time: 113 ms (17.28%) | Memory: 8.4 MB (70.68%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.go
+++ 287-find-the-duplicate-number/find-the-duplicate-number.go
@@ -1,10 +1,9 @@
 func findDuplicate(nums []int) int {
-   numMap := make(map[int]bool)
-    for i := 0 ; i < len(nums) ; i++ {
-        if numMap[nums[i]]{
+   sort.Ints(nums)
+    for i := 0 ; i < len(nums)-1 ; i++ {
+        if nums[i] == nums[i+1] {
             return nums[i]
         }
-        numMap[nums[i]] = true
     }
     return len(nums)
 }
