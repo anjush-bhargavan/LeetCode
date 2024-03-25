d453ae9c48806d751971d6ad7e72fed3a13189bc
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-25 10:25:03 +0530

    Time: 42 ms (74.22%) | Memory: 9.8 MB (5.47%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.go
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.go
@@ -1,12 +1,20 @@
 func findDuplicates(nums []int) []int {
     result := []int{}
     for i := 0 ; i < len(nums) ; i++ {
-        if nums[int(math.Abs(float64(nums[i])))-1] > 0 {
-            nums[int(math.Abs(float64(nums[i])))-1] *= -1
+        if nums[Abs(nums[i])-1] > 0 {
+            nums[Abs(nums[i])-1] *= -1
         }else{
-            result = append(result,int(math.Abs(float64(nums[i]))))
+            result = append(result,Abs(nums[i]))
     
         }
     }
     return result
+}
+
+
+func Abs(x int) int {
+    if x < 0 {
+        return x * -1
+    }
+    return x
 }
