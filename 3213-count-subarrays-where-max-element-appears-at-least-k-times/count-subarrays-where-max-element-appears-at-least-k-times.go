8805a08498d356ba9ab9f9b02c65c0ed8d75b1e4
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-29 13:27:48 +0530

    Time: 376 ms (12.50%) | Memory: 11.1 MB (12.50%) - LeetSync

---
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.go
@@ -0,0 +1,32 @@
+func countSubarrays(nums []int, k int) int64 {
+    max := MaxElement(nums)
+    countMax,count,i := 0,0,0
+    for j :=  range len(nums) {
+        if nums[j] == max {
+            countMax++
+        }
+        for countMax > k || (i <= j && countMax == k && nums[i] != max){
+            if nums[i] == max {
+                countMax--
+            }
+            i++
+        }
+        if countMax == k {
+            count += i+1
+            fmt.Println(i,count)
+        }
+    }
+    return int64(count)
+}
+
+
+
+func MaxElement(nums []int) int {
+    max := 0
+    for _,v := range nums {
+        if max < v {
+            max = v
+        }
+    }
+    return max
+}
