4c90c461cb8921eda0d062ac619087feabee2043
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-03-29 13:55:58 +0530

    Time: 122 ms (87.50%) | Memory: 9.5 MB (62.50%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.go
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.go
@@ -1,31 +1,27 @@
 func countSubarrays(nums []int, k int) int64 {
-    max := MaxElement(nums)
-    countMax,count,i := 0,0,0
+    countMax,count,i,max  := 0,0,0,0
     for j :=  range len(nums) {
+
+        if max < nums[j] {
+            max = nums[j]
+            countMax = 0
+            count = 0
+        }
+
         if nums[j] == max {
             countMax++
         }
+
         for countMax > k || (i <= j && countMax == k && nums[i] != max){
             if nums[i] == max {
                 countMax--
             }
             i++
         }
+
         if countMax == k {
             count += i + 1
         }
     }
     return int64(count)
-}
-
-
-
-func MaxElement(nums []int) int {
-    max := 0
-    for _,v := range nums {
-        if max < v {
-            max = v
-        }
-    }
-    return max
 }
