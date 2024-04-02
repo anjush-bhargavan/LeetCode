959dd003418feef0e7f316dd92b3c1d0a644f3a1
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-04-02 09:53:15 +0530

    Time: 2 ms (30.00%) | Memory: 2.4 MB (20.80%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.go
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sumNumbers(root *TreeNode) int {
+    arr := []int{}
+    sum,total := 0,0
+    PathSums(root,&arr,&sum)
+    fmt.Println(arr)
+    for _,s := range arr {
+        total += s
+    }
+    return total
+}
+
+
+func PathSums(root *TreeNode,arr *[]int,sum *int) {
+    if root != nil {
+        if root.Left == nil && root.Right == nil {
+            *sum = (*sum * 10) + root.Val
+            *arr = append(*arr,*sum)
+            *sum /= 10
+        }
+        *sum = (*sum * 10) + root.Val
+        PathSums(root.Left,arr,sum)
+        PathSums(root.Right,arr,sum)
+        *sum /= 10
+        
+    }
+}
