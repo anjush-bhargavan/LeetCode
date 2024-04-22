87b53bde51a39eae93839f73eb57f3628756b7c0
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-04-22 16:47:29 +0530

    Time: 3 ms (64.40%) | Memory: 2.7 MB (49.03%) - LeetSync

--- 61-rotate-list/rotate-list.go
+++ 61-rotate-list/rotate-list.go
@@ -9,24 +9,22 @@ func rotateRight(head *ListNode, k int) *ListNode {
     if k == 0 || head == nil {
         return head
     }
-    curr := head
+    curr,tail := head,head
     length := 0
     for curr != nil {
+        tail = curr
         curr = curr.Next
         length++
     }
+
     k = k % length
     curr = head
-    for _ = range k {
+    for _ = range length - k - 1 {
         curr = curr.Next
     }
-    curr1 := head
-    for curr.Next != nil {
-        curr = curr.Next
-        curr1 = curr1.Next
-    }
-    curr.Next = head
-    head = curr1.Next
-    curr1.Next = nil
+    
+    tail.Next = head
+    head = curr.Next
+    curr.Next = nil
     return head
 }
