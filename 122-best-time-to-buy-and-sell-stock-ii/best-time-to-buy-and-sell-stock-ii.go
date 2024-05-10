30dca00ada60ef6218847bb70bd1bb99cc039569
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-05-10 23:53:17 +0530

    Time: 2 ms (78.11%) | Memory: 3.4 MB (11.09%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
@@ -0,0 +1,9 @@
+func maxProfit(prices []int) int {
+    sum := 0
+    for i := 0 ; i < len(prices)-1 ; i++ {
+        if prices[i] < prices[i+1] {
+            sum += prices[i+1] - prices[i]
+        }
+    }
+    return sum
+}
