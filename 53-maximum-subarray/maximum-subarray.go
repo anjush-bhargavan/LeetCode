5bedd543a9666e99f320e078cb6612eb27085018
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-05-13 13:20:10 +0530

    Time: 88 ms (24.24%) | Memory: 12.9 MB (5.60%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.go
+++ 53-maximum-subarray/maximum-subarray.go
@@ -1,12 +1,10 @@
 func maxSubArray(nums []int) int {
-    i,max,sum := 1,nums[0],nums[0]
-
-    for i < len(nums) {
+    sum,max := nums[0],nums[0]
+    for i := 1 ; i < len(nums) ; i++ {
         if sum < 0 {
             sum = 0
         }
         sum += nums[i]
-        i++
         if max < sum {
             max = sum
         }
