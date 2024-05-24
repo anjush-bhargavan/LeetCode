d9e4938140cb01c319145494808d9a8fcae80e29
Anjush := <140878483+anjush-bhargavan@users.noreply.github.com>
2024-05-24 12:25:01 +0530

    Time: 0 ms (100.00%) | Memory: 2.4 MB (79.87%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.go
+++ 58-length-of-last-word/length-of-last-word.go
@@ -1,13 +1,13 @@
 func lengthOfLastWord(s string) int {
     count,flag := 0,false
-
     for i := len(s)-1 ; i >= 0 ; i-- {
-        if s[i] != 32 {
+        if s[i] != ' ' {
             count++
             flag = true
         }else if flag {
             break
         }
+
     }
     return count
 }
